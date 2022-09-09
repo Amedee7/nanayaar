e5e5ebbed3798475847cfcbe205b6dd6ce1df62a
Amedee7 <amedeeouedraogo01@gmail.com>
2022-09-09 13:43:18 +0000

    creation crud client

---
+++ .history/database/migrations/2022_09_08_193625_create_clients_table_20220908193722.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('numb_cli');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('first_phone');
+            $table->string('second_phone');
+            $table->date('date_naiss');
+            $table->string('numb_cnib');
+            $table->enum('genre', ['Masculin', 'Feminin'])->default('Masculin')->nullable();
+
+
+            $table->enum('status', ['accept', 'reject', 'pending'])->default('pending');
+
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->integer('created_by')->nullable()->default(1)->unsigned();
+
+            $table->integer('updated_by')->nullable()->default(1)->unsigned();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+};
---
+++ .history/resources/views/clients/modals/create.blade_20220907000733.php
@@ -0,0 +1,243 @@
+<div class="modal fade" id="modal" data-backdrop="static" >
+    <div class="modal-dialog modal-xl" client="document">
+        <div class="modal-content border-0 shadow radius-1">
+            <div class="modal-header">
+                <h5 class="modal-title text-primary-d2" id="exampleModal2Label">
+                    Ajouter un client
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">X</span>
+                </button>
+            </div>
+
+            <div class="modal-body modal-scroll">
+                <form action="{{route('clients.store')}}" method="POST" id="o2d-record-form-datatable">
+                    @csrf
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="card card-custom" style="border: solid; border-color: black">
+                                <div class="card-header">
+                                    <h5 class="card-title text-primary"><i class="fas fa-shield-alt"></i> Client</h5>
+                                </div>
+                                <div class="card-body">
+                                    <div class="form-group">
+                                    <label for=""><strong>NUMERO UNIQUE</strong></label>
+                                    <input name="numb_cli" type="text" class="form-control " id="id-form-field-1">
+                                    <span class="name_err error text-danger"></span>
+                                </div>
+                                    <div class="form-group">
+                                        <label for=""><strong>NOM</strong></label>
+                                        <input name="nom" type="text" class="form-control " id="id-form-field-1">
+                                        <span class="name_err error text-danger"></span>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for=""><strong>PRENOM</strong></label>
+                                        <input name="prenom" type="text" class="form-control " id="id-form-field-1">
+                                        <span class="name_err error text-danger"></span>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for=""><strong>TELEPHONE 1</strong></label>
+                                        <input name="first_phone" type="text" class="form-control " id="id-form-field-1">
+                                        <span class="name_err error text-danger"></span>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for=""><strong>TELEPHONE 2</strong></label>
+                                        <input name="second_phone" type="text" class="form-control " id="id-form-field-1">
+                                        <span class="name_err error text-danger"></span>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="">DATE DE NAISSANCE</label>
+                                        <input name="date_naiss" type="date" class="form-control " id="id-form-field-1">
+                                        <span class="display_name_err error text-danger"></span>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="">NUMERO CNIB</label>
+                                        <input name="numb_cnib" type="date" class="form-control " id="id-form-field-1">
+                                        <span class="display_name_err error text-danger"></span>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="">GENRE</label>
+                                        <select class="form-control select2" name="genre" id="mySelect2">
+                                            <option value="">--Veuillez choisir--</option>
+                                            @foreach($clients as $item)
+                                                <option value="{{$item->id}}">{{$item->genre}}</option>
+                                            @endforeach
+                                        </select>
+                                        <span class="role_err error text-danger"></span>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="">NUMERO CNIB</label>
+                                        <input name="numb_cnib" type="date" class="form-control " id="id-form-field-1">
+                                        <span class="display_name_err error text-danger"></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card card-custom" style="border: solid; border-color: black">
+                            <div class="card-header">
+                                <h5 class="card-title text-primary"><i class="fas fa-fingerprint"></i> Droits d'accès</h5>
+                            </div>
+                            <div class="card-body">
+                                <table class="table table-bordered">
+                                    <tr>
+                                        <th></th>
+                                        <th>Créer</th>
+                                        <th>Lire</th>
+                                        <th>Modifier</th>
+                                        <th>Supprimer</th>
+                                    </tr>
+                                    @foreach($modules as $module)
+                                        <tr>
+                                            <th>{{ ucfirst($module)}}</th>
+                                            <td>
+                                                @if (array_key_exists($module.'-creer',$permissions))
+                                                    <div class="checkbox-inline">
+                                                        <label class="checkbox checkbox-lg">
+                                                            <input type="checkbox" name="permissions[]" value="{{ $module.'-creer' }}" >
+                                                            <span></span>
+                                                        </label>
+                                                    </div>
+                                                @endif
+                                            </td>
+                                            <td>
+                                                @if (array_key_exists($module.'-lire',$permissions))
+                                                    <div class="checkbox-inline">
+                                                        <label class="checkbox checkbox-lg">
+                                                            <input type="checkbox" name="permissions[]" value="{{ $module.'-lire' }}" >
+                                                            <span></span>
+                                                        </label>
+                                                    </div>
+                                                @endif
+                                            </td>
+                                            <td>
+                                                @if (array_key_exists($module.'-modifier',$permissions))
+                                                    <div class="checkbox-inline">
+                                                        <label class="checkbox checkbox-lg">
+                                                            <input type="checkbox" name="permissions[]" value="{{ $module.'-modifier' }}" >
+                                                            <span></span>
+                                                        </label>
+                                                    </div>
+                                                @endif
+                                            </td>
+                                            <td>
+                                                @if (array_key_exists($module.'-supprimer',$permissions))
+                                                    <div class="checkbox-inline">
+                                                        <label class="checkbox checkbox-lg">
+                                                            <input type="checkbox" name="permissions[]" value="{{ $module.'-supprimer' }}" >
+                                                            <span></span>
+                                                        </label>
+                                                    </div>
+                                                @endif
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </table>
+                                <span class="permissions_err error text-danger"></span>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card card-custom" style="border: solid; border-color: black">
+                                <div class="card-header">
+                                    <h5 class="card-title text-primary"><i class="fas fa-fingerprint"></i> Droits d'accès particuliers</h5>
+                                </div>
+                                <div class="card-body row">
+
+                                    <div class="col-md-3">
+                                        <div class="checkbox-inline">
+                                            <label class="checkbox checkbox-lg">
+                                                <input type="checkbox" name="permissions[]" value="utilisateur-changer statut" >
+                                                <span></span>
+                                                Changer status utilisateur
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="checkbox-inline">
+                                            <label class="checkbox checkbox-lg">
+                                                <input type="checkbox" name="permissions[]" value="statistique-voir sur tableau de bord" >
+                                                <span></span>
+                                                Stats sur tableau de bord
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="checkbox-inline">
+                                            <label class="checkbox checkbox-lg">
+                                                <input type="checkbox" name="permissions[]" value="sous-operation-effectuer" >
+                                                <span></span>
+                                                Effectuer sous-opération
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="checkbox-inline">
+                                            <label class="checkbox checkbox-lg">
+                                                <input type="checkbox" name="permissions[]" value="configuration-regler" >
+                                                <span></span>
+                                                Régler configuration
+                                            </label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-3 mt-4">
+                                        <div class="checkbox-inline">
+                                            <label class="checkbox checkbox-lg">
+                                                <input type="checkbox" name="permissions[]" value="sauvegarde-creer" >
+                                                <span></span>
+                                                Effectuer une sauvegarde
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3 mt-4">
+                                        <div class="checkbox-inline">
+                                            <label class="checkbox checkbox-lg">
+                                                <input type="checkbox" name="permissions[]" value="sauvegarde-lire" >
+                                                <span></span>
+                                                Voir les sauvegardes
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3 mt-4">
+                                        <div class="checkbox-inline">
+                                            <label class="checkbox checkbox-lg">
+                                                <input type="checkbox" name="permissions[]" value="archive-lire" >
+                                                <span></span>
+                                                Voir les archives
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3 mt-4">
+                                        <div class="checkbox-inline">
+                                            <label class="checkbox checkbox-lg">
+                                                <input type="checkbox" name="permissions[]" value="log-lire" >
+                                                <span></span>
+                                                Voir les logs
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+            <div class="modal-footer bgc-default-l5">
+                <button type="button" id="sender" class="btn btn-primary" onclick="storeRecord();">
+                    <span id="btn-load" class="spinner-border d-none spinner-border-sm" client="status" aria-hidden="true"> </span>
+                    <b id="text-load" class="text-hide">Veuillez patienter</b>
+                    <b id="text-fix">Enregistrer</b>
+                </button>
+                <button type="button" class="btn btn-secondary px-4" data-dismiss="modal">
+                    Fermer
+                </button>
+            </div>
+        </div>
+    </div>
+</div>

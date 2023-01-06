3afcf2a61fbb227ac505264e428376cba9b8c7b6
Amedee7 <amedeeouedraogo01@gmail.com>
2023-01-06 15:03:36 +0000

    Updated 06-01-23-15h

---
+++ .history/app/Models/Versement_20230106101216.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use App\Models\Client;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Versement extends Model
+{
+    use HasFactory;
+    public $guarded = ['id'];
+
+    protected  static  function  boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->uuid = (string) Str::uuid();
+        });
+    }
+
+
+    public function client()
+    {
+        return $this->belongsToMany(Client::class, 'client_id', 'id');
+    }
+
+
+    public function calculatePenalty()
+{
+    $now = Carbon::now();
+    $deadline = Carbon::parse($this->limit);
+    $daysOverdue = $now->diffInDays($deadline);
+    $penaltyRate = config('app.penalty_rate');
+
+    if ($daysOverdue > 0) {
+        $this->penalite = $daysOverdue * $penaltyRate * $this->reste_apaye;
+    } else {
+        $this->penalite = 0;
+    }
+}
+}
